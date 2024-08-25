cfb07c76f7da6e022be201204c2aae06585f3ec0
Anna Fedorova <anna.fedorova.testing@gmail.com>
2024-08-25 17:42:57 +0300

    added .env file

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,36 +2,37 @@ import pytest
 from appium.options.android import UiAutomator2Options
 from selene import browser
 import os
-from selenium import webdriver
+from dotenv import load_dotenv
+from appium import webdriver
+
+
+@pytest.fixture(scope='session', autouse=True)
+def load_env():
+    load_dotenv()
 
 
 @pytest.fixture(scope='function', autouse=True)
 def mobile_management():
+    login = os.getenv('USER_NAME')
+    access_key = os.getenv('ACCESS_KEY')
     options = UiAutomator2Options().load_capabilities({
-        # Specify device and os_version for testing
-        # "platformName": "android",
         'platformVersion': '9.0',
         'deviceName': 'Google Pixel 3',
 
-        # Set URL of the application under test
         'app': 'bs://sample.app',
 
-        # Set other BrowserStack capabilities
         'bstack:options': {
             'projectName': 'First Python project',
             'buildName': 'browserstack-build-1',
             'sessionName': 'BStack first_test',
 
-            # Set your access credentials
-            'userName': 'anna_AdBgcK',
-            'accessKey': 'n9u5fcs9D7VG85hzWbwU'
+            'userName': login,
+            'accessKey': access_key
         }
     })
 
-    # browser.config.driver = webdriver.Remote("http://hub.browserstack.com/wd/hub", options=options)
     browser.config.driver_remote_url = 'http://hub.browserstack.com/wd/hub'
     browser.config.driver_options = options
-
 
     browser.config.timeout = float(os.getenv('timeout', '10.0'))
 
